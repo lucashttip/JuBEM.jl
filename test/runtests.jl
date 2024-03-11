cdbc2971725753077d8c1125fd7a5aa5599654f4
Lucas Pacheco <lucas.agattipacheco@gmail.com>
2024-03-11 01:53:08 -0300

    added testset for soil dyn

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
 using JuBEM
 using Test
-
-
+using LinearAlgebra
 
 function teste_bar_io(inp_file)
 
@@ -42,7 +41,6 @@ function teste_bar_static(inp_file, t=0)
 
 end
 
-
 function teste_bar_static2(inp_file, t=0)
 
     mesh, material, problem, solver_var = read_msh(inp_file)
@@ -63,6 +61,31 @@ function teste_bar_static2(inp_file, t=0)
 
 end
 
+function teste_soil_dyn(inp_file)
+
+
+    solve_flex_dyn(inp_file;file_out = "test_output")
+    N, freqs = getflex_out("test_output")
+    rm("test_output.h5", force=true)
+
+
+    return  N, freqs
+
+end
+
+function teste_soil_dyn2(inp_file)
+
+
+    JuBEM.solve_flex_dyn2(inp_file;file_out = "test_output")
+    N, freqs = getflex_out("test_output")
+    rm("test_output.h5", force=true)
+
+
+    return  N, freqs
+
+end
+
+
 @testset "IO" begin
     inp_file = "../meshes/static/bars/bar_2_3.msh"
     ud = teste_bar_io(inp_file)
@@ -80,4 +103,15 @@ end
     @test abs(ud_const-1) < 0.5
     @test abs(ud_lin-1) < 0.02
 
+end
+
+@testset "soil_dyn" begin
+
+    inp_file = "./meshes/dynamic/soils/soilEE_109_rb.msh"
+    ref_file = "./ref_res/test_output"
+
+    N2,freqs = teste_soil_dyn2(inp_file)
+    N,freqs = getflex_out(ref_file)
+
+    @test norm(N - N2) < 1e-4
 end
